69263623b5058a12545918021413af7e8f6d9f89
GoodWitchLalya <72929397+GoodWitchLalya@users.noreply.github.com>
2026-01-29 17:30:56 +0100

    Site completed

--- satellite-star/astro.config.mjs
+++ satellite-star/astro.config.mjs
@@ -6,10 +6,19 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
     site: 'https://goodwitchlalya.github.io',
     base: '/Lalyan-Documentation',
+
+    redirects: {
+        '/': '/Lalyan-Documentation/en/',
+    },
+
     integrations: [
        starlight({
           title: 'Lalyan Documentation',
-          // Modificato qui: ora è una lista [] invece di un oggetto {}
+          defaultLocale: 'en',
+          locales: {
+              en: { label: 'English', lang: 'en' },
+              it: { label: 'Italiano', lang: 'it' },
+          },
           social: [
              {
                 icon: 'github',
